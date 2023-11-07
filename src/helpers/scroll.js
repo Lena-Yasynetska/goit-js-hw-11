493b683abda6c778ca5a9880b06e825e11a29785
Lena-Yasynetska <123744975+Lena-Yasynetska@users.noreply.github.com>
2023-11-07 23:47:55 +0100

    11/13

--- src/helpers/scroll.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector(".search-input");
-
-export const btnUp = document.querySelector('.go-up');
-btnUp.addEventListener('click', () => {
-   window.scrollTo(0, 0);
-   input.value = "";
-});
